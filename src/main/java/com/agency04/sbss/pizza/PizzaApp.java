887c818c5b78f0ba6574e2918c6da973476ecd9f
kmandalinic <katarina.mandalinic@gmail.com>
2021-08-03 10:02:34 +0200

    Solve week-2 part 1

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -3,6 +3,7 @@ package com.agency04.sbss.pizza;
 import com.agency04.sbss.pizza.model.Pizza;
 import com.agency04.sbss.pizza.model.PizzaMargherita;
 import com.agency04.sbss.pizza.service.PizzaDeliveryService;
+import com.agency04.sbss.pizza.service.PizzeriaService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class PizzaApp {
@@ -12,8 +13,19 @@ public class PizzaApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         Pizza margherita = new PizzaMargherita();
 
-        PizzaDeliveryService pizzaDeliveryService = context.getBean("pizzaDeliveryServiceImpl", PizzaDeliveryService.class);
-        System.out.println(pizzaDeliveryService.orderPizza(margherita));
+        
+        PizzeriaService pizzeriaService = context.getBean("pizzeriaNumeroUnoService", PizzeriaService.class);
+        PizzeriaService pizzeriaService1 = context.getBean("pizzeriaNumeroUnoService", PizzeriaService.class);
+        boolean result = (pizzeriaService==pizzeriaService1);
+        System.out.println(result);
+        System.out.println("Memory location for pizzeriaService: " + pizzeriaService);
+        System.out.println("Memory location for pizzeriaService: " + pizzeriaService1);
+        /*
+        Default scope is singleton, they are pointing to the same object (memory location is the same)
+        when we change scope to prototype, two objects are created (different memory location)
+        Prototype: new object is created each time it's injected, Singleton: same object is returned each time
+        Note: prototype-scoped objects are not destroyed by default like singleton-scoped objects
+        */
 
         context.close();
     }
--- src/main/java/com/agency04/sbss/pizza/service/PizzeriaNumeroUnoService.java
+++ src/main/java/com/agency04/sbss/pizza/service/PizzeriaNumeroUnoService.java
@@ -2,6 +2,7 @@ package com.agency04.sbss.pizza.service;
 
 import com.agency04.sbss.pizza.model.Pizza;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +10,7 @@ import javax.annotation.PreDestroy;
 import java.util.Locale;
 
 @Component
+@Scope("prototype")
 public class PizzeriaNumeroUnoService implements PizzeriaService {
     @Value("${uno.name}")
     private String name;
